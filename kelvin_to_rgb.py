923ad6104cbd34bfaa41afe6be7b111e688b6bd6
rhinot <rhinot@users.noreply.github.com>
2017-05-14 23:27:23 -0400

    Update and rename rgb_to_kelvin.py to kelvin_to_rgb.py
    Update name to reflect what's happening, remove plotting function & americanize language

--- kelvin_to_rgb.py
+++ kelvin_to_rgb.py
@@ -55,12 +55,12 @@ def convert_K_to_RGB(colour_temperature):
     http://www.tannerhelland.com/4435/convert-temperature-rgb-algorithm-code/
     """
     #range check
-    if colour_temperature < 1000: 
-        colour_temperature = 1000
-    elif colour_temperature > 40000:
-        colour_temperature = 40000
+    if color_temperature < 1000: 
+        color_temperature = 1000
+    elif color_temperature > 40000:
+        color_temperature = 40000
     
-    tmp_internal = colour_temperature / 100.0
+    tmp_internal = color_temperature / 100.0
     
     # red 
     if tmp_internal <= 66:
@@ -108,18 +108,5 @@ def convert_K_to_RGB(colour_temperature):
     
     return red, green, blue
     
-
-
-if __name__ == "__main__":
-    print("Preview requires matplotlib")
-    from matplotlib import pyplot as plt
-
-    step_size = 100
-    for i in range(0, 15000, step_size):
-        color = list(map(lambda div: div/255.0, convert_K_to_RGB(i))) + [1]
-        print(color)
-        plt.plot((i, i), (0, 1), linewidth=step_size/2.0, linestyle="-", color=color)
-    
-    plt.show()  
 
 
